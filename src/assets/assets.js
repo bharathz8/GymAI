072c553935f636a165173ffe80d247446c30acac
Bharath kumar G 39 bca <bharathkumarg668@gmail.com>
2024-12-17 13:17:10 +0530

    compress the images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,14 +1,14 @@
 import BlackLogo from "./images/blackLogo.png";
 import WhiteLogo from "./images/whiteLogo.png";
 import hamburger from "./icons/hamburger.svg"
-import CountReps from "./images/CountReps.jpg";
-import LogExercise from "./images/LogExercise.jpg"
-import ReviewImg from "./images/Review.jpg";
-import TrackProgress from "./images/TrackProgress.jpg";
-import Gym from "./images/gym.jpg";
-import fitness from "./images/fitness.jpg";
-import boxing from "./images/boxing.jpg";
-import yoga from "./images/yoga.jpg"
+import CountReps from "./images/CountReps.jpeg";
+import LogExercise from "./images/LogExercise.jpeg"
+import ReviewImg from "./images/Review.jpeg";
+import TrackProgress from "./images/TrackProgress.jpeg";
+import Gym from "./images/gym.jpeg";
+import fitness from "./images/fitness.jpeg";
+import boxing from "./images/boxing.jpeg";
+import yoga from "./images/yoga.jpeg"
 
 export {
     BlackLogo, WhiteLogo, hamburger, CountReps, LogExercise, ReviewImg, TrackProgress, Gym, fitness, boxing, yoga
