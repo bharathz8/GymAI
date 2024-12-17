614770e4c6b8951c96ef93df0d8d275a3ef1f11f
Bharath kumar G 39 bca <bharathkumarg668@gmail.com>
2024-12-17 09:55:28 +0530

    changes in video

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,7 +6,7 @@ import LogExercise from "./images/LogExercise.jpg"
 import ReviewImg from "./images/Review.jpg";
 import TrackProgress from "./images/TrackProgress.jpg";
 import heromp4 from "./video/hero.mp4";
-import herowebm from "./icons/hero.webm";
+import herowebm from "./video/hero.webm";
 
 export {
     BlackLogo, WhiteLogo, hamburger, CountReps, LogExercise, ReviewImg, TrackProgress, heromp4, herowebm
